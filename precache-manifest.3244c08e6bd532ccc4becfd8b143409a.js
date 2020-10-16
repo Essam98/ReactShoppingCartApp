a99583256ad29153372fdf017237ae03c32c2cec
Essam98 <Essam98@gmail.com>
2020-10-16 14:05:11 +0300

    Updates

---
+++ precache-manifest.3244c08e6bd532ccc4becfd8b143409a.js
@@ -0,0 +1,166 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85bf2efddde704c99c66c8928e5d2caa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "08b497d8ce7ae9b3c4c7",
+    "url": "/static/css/2.42a5fdae.chunk.css"
+  },
+  {
+    "revision": "99141314579d0ceb78d8",
+    "url": "/static/css/main.5588c2d7.chunk.css"
+  },
+  {
+    "revision": "08b497d8ce7ae9b3c4c7",
+    "url": "/static/js/2.faa252ef.chunk.js"
+  },
+  {
+    "revision": "a8f28da15ef57bb0c0b949e14935b0a3",
+    "url": "/static/js/2.faa252ef.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "99141314579d0ceb78d8",
+    "url": "/static/js/main.62ae52ba.chunk.js"
+  },
+  {
+    "revision": "534a3a78d30bd5c0bdc2",
+    "url": "/static/js/runtime-main.795e4ebc.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "66e74208377c8370afe6a5c858635c4d",
+    "url": "/static/media/more1.66e74208.jpg"
+  },
+  {
+    "revision": "ea61b54ca0247ea3f26c37bd5bf3c969",
+    "url": "/static/media/more10.ea61b54c.jpg"
+  },
+  {
+    "revision": "ed445ce29bb80fb88b121efa73ff0e6c",
+    "url": "/static/media/more12.ed445ce2.jpg"
+  },
+  {
+    "revision": "1f969ebe3e5f71c2091e55e0f146e73a",
+    "url": "/static/media/more14.1f969ebe.jpg"
+  },
+  {
+    "revision": "102876603293c95d5fe44359214a1ae1",
+    "url": "/static/media/more26.10287660.jpg"
+  },
+  {
+    "revision": "6e756200510e34fe9fd066b8e55d9542",
+    "url": "/static/media/more3.6e756200.jpg"
+  },
+  {
+    "revision": "89ac34a06d8bebf1ebfdbc73f609f9a7",
+    "url": "/static/media/more4.89ac34a0.jpg"
+  },
+  {
+    "revision": "f356609b1008ac1605561ad6504a27e6",
+    "url": "/static/media/more5.f356609b.jpg"
+  },
+  {
+    "revision": "6876360420d692384df0717ad1c16bc8",
+    "url": "/static/media/more7.68763604.jpg"
+  },
+  {
+    "revision": "be103e672c6703de20d7e12e2202d0e3",
+    "url": "/static/media/more8.be103e67.jpg"
+  },
+  {
+    "revision": "0e4dc06df85a3e849e9a065a1b23b11b",
+    "url": "/static/media/more9.0e4dc06d.jpg"
+  },
+  {
+    "revision": "e14c2e8a3ad7edda7b2c712d3ac49591",
+    "url": "/static/media/product_1.e14c2e8a.jpg"
+  },
+  {
+    "revision": "4bf4023ab43b659680409790ddebcd90",
+    "url": "/static/media/product_10.4bf4023a.jpg"
+  },
+  {
+    "revision": "abe9908cb2ea5d2da0f9b979bfd3ba28",
+    "url": "/static/media/product_11.abe9908c.jpg"
+  },
+  {
+    "revision": "58b75fbff7a72a8bd21b365b142180bc",
+    "url": "/static/media/product_12.58b75fbf.jpg"
+  },
+  {
+    "revision": "49ee2d4c5c742e45901660c11ebc5b90",
+    "url": "/static/media/product_13.49ee2d4c.jpg"
+  },
+  {
+    "revision": "66906463e80f8f8fab794c3c3bebc99b",
+    "url": "/static/media/product_14.66906463.jpg"
+  },
+  {
+    "revision": "cb660f05f0bd206f96a4762b93823c9d",
+    "url": "/static/media/product_15.cb660f05.jpg"
+  },
+  {
+    "revision": "bd426ead8c3e4c726e3356e31a7bfc9c",
+    "url": "/static/media/product_16.bd426ead.jpg"
+  },
+  {
+    "revision": "27ae4487cdba7e15a31b19005e0beee0",
+    "url": "/static/media/product_2.27ae4487.jpg"
+  },
+  {
+    "revision": "e44478a819e6e3c37adfd092f76247da",
+    "url": "/static/media/product_3.e44478a8.jpg"
+  },
+  {
+    "revision": "e8a89ae22ad5e2830a79fc010c9c36cd",
+    "url": "/static/media/product_4.e8a89ae2.jpg"
+  },
+  {
+    "revision": "c76f9beaaf53dc2db9fddebf592de2ec",
+    "url": "/static/media/product_5.c76f9bea.jpg"
+  },
+  {
+    "revision": "da4335883f994d314c229641a9bcf8a2",
+    "url": "/static/media/product_6.da433588.jpg"
+  },
+  {
+    "revision": "ca5e78cb7af639cf3bb4f01bacea8b57",
+    "url": "/static/media/product_7.ca5e78cb.jpg"
+  },
+  {
+    "revision": "d5e645b4e7de1d4b90c41fb408d25f7d",
+    "url": "/static/media/product_8.d5e645b4.jpg"
+  },
+  {
+    "revision": "996453def0bb9e59f7582ee948bc0606",
+    "url": "/static/media/product_9.996453de.jpg"
+  },
+  {
+    "revision": "7c6383c6549590236f362ec47d3a70bb",
+    "url": "/static/media/slider1.7c6383c6.jpg"
+  },
+  {
+    "revision": "977967eff984b0605363cf04e48fe9a7",
+    "url": "/static/media/slider3.977967ef.jpg"
+  }
+]);
